23ef46296850a46d84fd5ee189772b2d9551e670
Yesu <yesupadam2001@gmail.com>
2023-11-25 10:28:26 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Translate Web APP
+
+Built with:
+
+- React + Vite
+- Tailwind CSS
+
+Demo: [Translate App]()
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import "./App.css";
+function App() {
+  return <div className="h-full pb-[100px]">hello</div>;
+}
+
+export default App;
